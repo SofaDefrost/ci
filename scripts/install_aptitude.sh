0b3688272c90f43689686b15bc3bea9e23b00fff
jackdefrost <>
2019-01-08 17:13:44 +0100

    Adding required packages for sofa install on the CI

---
+++ scripts/install_aptitude.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt-get install build-essential libboost-all-dev cmake ninja-build python2.7-dev python-numpy python-scipy libpng-dev libjpeg-dev libtiff-dev zlib1g-dev libglew-dev qt5-default qtquickcontrols2-5-dev
